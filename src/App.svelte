5de74b0703589beb065146bca5beafe94a339a04
jimmycliff obonyo <cliffjimmy27@gmail.com>
2021-02-11 23:20:28 +0300

    init

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Dashboard from './components/dashboard.svelte';
+	export let name;
+</script>
+
+<Dashboard />
+
+
+<style global lang="postcss">
+  /* only apply purgecss on utilities, per Tailwind docs */
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+
+</style>
